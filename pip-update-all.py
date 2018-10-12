7d04492a0815ca36d04b0eab137a96512b527422
makarandh <38956972+makarandh@users.noreply.github.com>
2018-10-12 15:43:24 +0530

    changed commands to python3

--- pip-update-all.py
+++ pip-update-all.py
@@ -7,19 +7,17 @@
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see https://www.gnu.org/licenses/.
 
-
 import os
-
 
 def update_psw():
     # Updating pip, setuptools, and wheel
     print("")
     print("Updating pip...")
-    os.system("python -m pip install --upgrade pip")
+    os.system("python3 -m pip install --upgrade pip")
     print("Updating setuptools...")
-    os.system("python -m pip install --upgrade setuptools")
+    os.system("python3 -m pip install --upgrade setuptools")
     print("Updating wheel...")
-    os.system("python -m pip install --upgrade wheel")
+    os.system("python3 -m pip install --upgrade wheel")
 
 
 def update_outdated_packages():
@@ -28,7 +26,7 @@ def update_outdated_packages():
     print("")
 
     # Querying outdated packages
-    output = os.popen("pip list --outdated").read()
+    output = os.popen("pip3 list --outdated").read()
     print("")
 
     # There are three lines that doesn't contain package info and can be ignored
@@ -49,7 +47,7 @@ def update_outdated_packages():
         end = line.find(" ")
         package = line[:end]
         print("Updating package", i, "of", count, " " + str(package) + "...")
-        os.system("pip install --upgrade " + str(package))
+        os.system("pip3 install --upgrade " + str(package))
 
 
 if __name__ == "__main__":
